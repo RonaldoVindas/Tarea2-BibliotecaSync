e0edb30659bad7dd80b9abc51b5cb9d2506aad2c
Rebbox <37891786+RemmosLost@users.noreply.github.com>
2024-03-17 23:50:41 -0600

    Correcciones de Semaphore

--- PruebaSemaphore.c
+++ PruebaSemaphore.c
@@ -1,66 +1,43 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <stdlib.h>
-
-typedef struct {
-    int value;
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-} Semaphore;
-
-Semaphore* createSemaphore(int initialValue) {
-    Semaphore* semaphore = (Semaphore*)malloc(sizeof(Semaphore));
-    if (semaphore == NULL) {
-        perror("Error al crear el semáforo");
-        exit(EXIT_FAILURE);
-    }
-    semaphore->value = initialValue;
-    pthread_mutex_init(&(semaphore->mutex), NULL);
-    pthread_cond_init(&(semaphore->cond), NULL);
-    return semaphore;
-}
-
-void destroySemaphore(Semaphore* semaphore) {
-    pthread_mutex_destroy(&(semaphore->mutex));
-    pthread_cond_destroy(&(semaphore->cond));
-    free(semaphore);
-}
-
-void waitSemaphore(Semaphore* semaphore) {
-    pthread_mutex_lock(&(semaphore->mutex));
-    while (semaphore->value <= 0) {
-        pthread_cond_wait(&(semaphore->cond), &(semaphore->mutex));
-    }
-    semaphore->value--;
-    pthread_mutex_unlock(&(semaphore->mutex));
-}
-
-void signalSemaphore(Semaphore* semaphore) {
-    pthread_mutex_lock(&(semaphore->mutex));
-    semaphore->value++;
-    pthread_cond_signal(&(semaphore->cond));
-    pthread_mutex_unlock(&(semaphore->mutex));
-}
-
+#include "semaphore.h"
+#include "semaphore.c"
+#include <unistd.h>
 #define NUM_THREADS 5
 
-Semaphore* semaphore;
+
+typedef struct {
+    int id;
+    Semaphore* semaphore;
+} ThreadArgs;
+
 
 void* thread_function(void* arg) {
-    printf("Hilo %ld esperando en el semáforo\n", pthread_self());
-    waitSemaphore(semaphore);
-    printf("Hilo %ld liberado del semáforo\n", pthread_self());
+    ThreadArgs* args = (ThreadArgs*)arg;
+    printf("Hilo %d esperando en el semáforo\n", args->id);
+    waitSemaphore(args->semaphore); 
+    printf("Hilo %d adquirió el semáforo\n", args->id);
+    printf("Hilo %d realizando trabajo dentro de la sección crítica\n", args->id);
+    sleep(1);
+    signalSemaphore(args->semaphore); 
+    printf("Hilo %d liberó el semáforo\n", args->id);
     return NULL;
 }
 
 int main() {
-    semaphore = createSemaphore(1);
     pthread_t threads[NUM_THREADS];
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_create(&threads[i], NULL, thread_function, NULL);
-    }
+    ThreadArgs thread_args[NUM_THREADS];
+    Semaphore* semaphore = createSemaphore(2); 
 
     for (int i = 0; i < NUM_THREADS; i++) {
-        
+        thread_args[i].id = i + 1;
+        thread_args[i].semaphore = semaphore;
+        pthread_create(&threads[i], NULL, thread_function, (void*)&thread_args[i]);
     }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    destroySemaphore(semaphore); 
+
+    return 0;
 }
